89b5595b9e5f97223247f6bfeeff317d10fbab01
Adriano <adrianotagliaferro@hotmail.com>
2020-06-29 16:39:06 -0300

    Implemented view binding.

--- app/src/main/java/br/com/tagliaferro/soccerplayers/fragment/LoginFragment.kt
+++ app/src/main/java/br/com/tagliaferro/soccerplayers/fragment/LoginFragment.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import br.com.tagliaferro.soccerplayers.R
+import br.com.tagliaferro.soccerplayers.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var binding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        binding = LoginFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(view.context, "Teste with view binding", Toast.LENGTH_SHORT).show()
+        }
     }
 }
